f29343b81d5fa483b1f38f42292fa243c671a318
NataMalysheva <shnauzer@gmail.com>
2025-04-13 21:39:21 +0300

    work

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,40 @@
-//Задача 3. Перевірка спаму
-function checkForSpam(message) {
-  const lowerMessage = message.toLowerCase();
-  return lowerMessage.includes('spam') || lowerMessage.includes('sale');
+/* Задача 3. Фільтрація масиву чисел
+
+Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) 
+та значення (value) як параметри. Функція повинна повертати новий масив 
+лише тих чисел із масиву numbers, які більші за значення value.
+
+Усередині функції:
+
+Створи порожній масив, у який будеш додавати підходящі числа.
+Використай цикл для ітерації кожного елемента масиву numbers.
+Використовуй умовний оператор if усередині циклу для перевірки 
+кожного елемента и додавання до свого масиву.
+Поверни свій новий масив з підходящими числами як результат.
+
+Візьми код нижче і встав після оголошення своєї функції для перевірки 
+коректності її роботи. У консоль будуть виведені результати її роботи.
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+*/
+
+function filterArray(numbers, value) {
+  const result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
+    }
+  }
+  return result;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// Перевірка:
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
